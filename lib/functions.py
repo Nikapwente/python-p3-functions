9e401ab4258c378e87e28167aae8f60bbcc1828e
Nikapwente <hrsmajor@gmail.com>
2023-12-05 17:28:23 +0300

    Assignment Complete

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,28 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+    return None
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+    return None
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+    return None
 
 def add(num1, num2):
     pass
+    return num1 + num2 
 
 def halve(number):
     pass
+    if type(number) == int :
+        return number/2
+    elif type(number) == float:
+        return number/2
+    else:
+        return None
